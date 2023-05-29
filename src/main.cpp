2c5fb8e4b5c18f3c4509e3823eb08526ebb0e3e0
Alexandr Ponomarev <ponomarev.alex8146@gmail.com>
2023-05-29 21:07:13 +0300

    Добавил отслеживание зармеров окна и добавли закрытие окна с помолью escape

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,50 @@
 
 using namespace std;
 
+
+int windowSizeX=640;
+int windowSizeY=480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
+{
+    windowSizeX = width;
+    windowSizeY = height;
+    glViewport(0, 0, windowSizeX, windowSizeY);
+}
+
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scanecode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+}
+
+
 int main(void)
 {
-    GLFWwindow* window;
-
+    
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    //State version glfw
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(windowSizeX, windowSizeY, "Let the BAttle Begin", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -29,9 +58,12 @@ int main(void)
         return -1;
     }
 
-    cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << endl;
 
-    glClearColor(0, 1,0 ,1);
+    cout << "Render: " << glGetString(GL_RENDERER) << endl;
+    cout << "Version GL: " << glGetString(GL_VERSION) << endl;
+
+
+    glClearColor(1, 1,0 ,1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
